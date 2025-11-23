6ee54d0feeca58d68decc3c99e238565e7d8eff1
gunjan85330 <gunjan.singh_bsc.math23@gla.ac.in>
2025-11-23 14:56:41 +0530

    Update Login.py

--- app/Login.py
+++ app/Login.py
@@ -1,1 +1,16 @@
+import streamlit as st
+from app.utils.auth import check_login
+
+st.title(" Login to AI-Forest Dashboard")
+
+username = st.text_input("Username")
+password = st.text_input("Password", type="password")
+
+if st.button("Login"):
+    if check_login(username, password):
+        st.session_state["logged_in"] = True
+        st.success("Login successful!")
+        st.switch_page("app/Home.py")
+    else:
+        st.error("Invalid username or password")
 
