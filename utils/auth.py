e95c01d1a13e5855ab0edb2703f79b50dbf7fe58
gunjan85330 <gunjan.singh_bsc.math23@gla.ac.in>
2025-11-23 15:41:10 +0530

    Update auth.py

--- utils/auth.py
+++ utils/auth.py
@@ -1,6 +1,4 @@
 def check_login(username, password):
-    VALID_USER = "gunjan_singh"
-    VALID_PASS = "8533099"
+    return username == "gunjan_singh" and password == "8533099"
 
-    return username == VALID_USER and password == VALID_PASS
 
