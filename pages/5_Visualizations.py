578221184c53b826e7b70c4acc939d63a3b719f1
gunjan85330 <gunjan.singh_bsc.math23@gla.ac.in>
2025-11-23 15:39:24 +0530

    Update 5_Visualizations.py

--- pages/5_Visualizations.py
+++ pages/5_Visualizations.py
@@ -1,20 +1,14 @@
 import streamlit as st
 import pydeck as pdk
-from app.utils.model_loader import load_dataset, load_model
+from utils.model_loader import load_dataset, load_model
 
-if "logged_in" not in st.session_state:
-    st.switch_page("app/Login.py")
-
-st.title(" Risk Heatmaps")
+st.title("Fire Risk Heatmap")
 
 df = load_dataset()
 model = load_model()
 
 FEATURES = model.feature_names_in_
-
 df["risk"] = model.predict_proba(df[FEATURES].fillna(0))[:,1]
-
-st.subheader("Fire Risk Heatmap")
 
 layer = pdk.Layer(
     "HeatmapLayer",
@@ -24,11 +18,13 @@ layer = pdk.Layer(
     radiusPixels=40,
 )
 
-view = pdk.ViewState(
-    latitude=df["lat"].mean(),
+view_state = pdk.ViewState(
     longitude=df["lon"].mean(),
+    latitude=df["lat"].mean(),
     zoom=7,
 )
 
-st.pydeck_chart(pdk.Deck(layers=[layer], initial_view_state=view))
-
+st.pydeck_chart(pdk.Deck(
+    layers=[layer],
+    initial_view_state=view_state
+))
