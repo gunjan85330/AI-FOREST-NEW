d47bf37a73057707947063b7b1398fb1956c240f
gunjan85330 <gunjan.singh_bsc.math23@gla.ac.in>
2025-11-23 15:42:03 +0530

    Update simulation.py

--- utils/simulation.py
+++ utils/simulation.py
@@ -2,27 +2,23 @@ import numpy as np
 from sklearn.neighbors import KDTree
 
 def run_simulation(df, model, lat, lon, steps, threshold):
-
     coords = df[["lat", "lon"]].values
     tree = KDTree(coords)
 
     dist = np.abs(df["lat"] - lat) + np.abs(df["lon"] - lon)
     start_idx = dist.idxmin()
 
+    FEATURES = model.feature_names_in_
+
     burning = {start_idx}
     history = [burning.copy()]
 
-    FEATURES = model.feature_names_in_
-
     for _ in range(steps):
-
         new_fire = set()
         for cell in burning:
-
-            _, neighbors = tree.query([coords[cell]], k=50)
-            for n in neighbors[0][1:]:
-
-                risk = model.predict_proba(df.loc[n, FEATURES].to_frame().T.fillna(0))[0][1]
+            _, neigh = tree.query([coords[cell]], k=50)
+            for n in neigh[0][1:]:
+                risk = model.predict_proba(df.loc[n, FEATURES].to_frame().T)[0][1]
                 if risk > threshold:
                     new_fire.add(n)
 
@@ -30,4 +26,5 @@ def run_simulation(df, model, lat, lon, steps, threshold):
         history.append(burning.copy())
 
     return history, len(burning)
+
 
