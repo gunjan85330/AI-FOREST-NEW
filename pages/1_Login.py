f5e9be4cfb8d2de4a63b369e54d74f1273425668
gunjan85330 <gunjan.singh_bsc.math23@gla.ac.in>
2025-11-23 15:37:44 +0530

    Update and rename 2_Login.py to 1_Login.py

--- pages/1_Login.py
+++ pages/1_Login.py
@@ -1,16 +1,15 @@
 import streamlit as st
-from app.utils.auth import check_login
+from utils.auth import check_login
 
-st.title(" Login to AI-Forest Dashboard")
+st.title("Login")
 
 username = st.text_input("Username")
 password = st.text_input("Password", type="password")
 
 if st.button("Login"):
     if check_login(username, password):
-        st.session_state["logged_in"] = True
         st.success("Login successful!")
-        st.switch_page("app/Home.py")
     else:
         st.error("Invalid username or password")
+
 
