f69c634c70be27d8e926448056c6363e75c192bf
gunjan85330 <gunjan.singh_bsc.math23@gla.ac.in>
2025-11-23 14:55:38 +0530

    Update streamlit_app.py

--- AI-Forest/streamlit_app.py
+++ AI-Forest/streamlit_app.py
@@ -1,1 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="AIFOREST | Wildfire System",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+st.logo("https://i.imgur.com/ZUMQfrQ.png")
+
+st.sidebar.success("Select a page above to continue.")
 
