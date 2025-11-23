14cd917c3b521b715f654f19367e2a4906dd8421
gunjan85330 <gunjan.singh_bsc.math23@gla.ac.in>
2025-11-23 15:00:45 +0530

    Update Contact.py

--- app/Contact.py
+++ app/Contact.py
@@ -1,1 +1,13 @@
+import streamlit as st
+
+if "logged_in" not in st.session_state:
+    st.switch_page("app/Login.py")
+
+st.title(" Contact Us")
+
+st.markdown("""
+For project queries or collaboration, you can reach us at:
+
+**gunjan.singh_bsc.math23@gla.ac.in**
+""")
 
